b87ce04bbaee2eef3ada776d95c1fb218682c977
Greg <greg.malek@gmail.com>
2020-09-18 16:56:25 +0200

    Angulat Created add function

--- todo-Angular/src/app/components/todo-list/todo-list.component.ts
+++ todo-Angular/src/app/components/todo-list/todo-list.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
+  todoTitle: string;
+  idForTodo: number;
 
+  //here we declare our classes
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  //constructor
+    this.idForTodo = 4;
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'complited': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over the world',
+        'complited': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'One mor thing',
+        'complited': false,
+        'editing': false,
+      },
+    ];
   }
 
+  addTodo(): void {
+    if(this.todoTitle.trim().length == 0)
+    {
+      return;
+    }
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      complited: false,
+      editing: false
+    })
+
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
 }
